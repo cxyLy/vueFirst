85ebe44de328dc970ee267806d19ecda463c905e
caoxiangyang <caoxiangyang@oschina.cn>
2021-10-18 15:47:21 +0800

    测试空格

--- white_space.js
+++ white_space.js
@@ -1,5 +1,5 @@
-export const isMac = () => {
-  const UAInfo = window.navigator.userAgent
+  export const isMac = () => {  
+    const UAInfo = window.navigator.userAgent 
 
   if (UAInfo.indexOf('Mac OS') > -1) {
     return true
@@ -7,10 +7,11 @@ export const isMac = () => {
   return false
 }
 
-export function clearSelections() {
-  if (window.getSelection) {
+ export function clearSelections() { 
+   if (window.getSelection) { 
     window.getSelection().removeAllRanges()
   } else if (document.selection && document.selection.empty) {
-    document.selection.empty()
+    doc ument.selection.empty()
   }
+  
 }
