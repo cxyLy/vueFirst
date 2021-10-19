884569f5f614bde0d4bdc239d6fcf68b5353d400
caoxiangyang <caoxiangyang@oschina.cn>
2021-10-19 13:08:02 +0800

    编辑white——space

--- white_space.js
+++ white_space.js
@@ -14,3 +14,19 @@ export function clearSelections() {
     document.selection.empty()
   }
 }
+export const isMac = () => {
+  const UAInfo = window.navigator.userAgent
+
+  if (UAInfo.indexOf('Mac OS') > -1) {
+    return true
+  }
+  return false
+}
+
+export function clearSelections() {
+  if (window.getSelection) {
+    window.getSelection().removeAllRanges()
+  } else if (document.selection && document.selection.empty) {
+    document.selection.empty()
+  }
+}
